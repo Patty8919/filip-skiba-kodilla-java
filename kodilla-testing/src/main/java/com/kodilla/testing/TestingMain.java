6348d62b99c7eded08de7c91cfcde2646582cfe9
filipskiba <filipskiba.247@gmail.com>
2019-06-12 07:17:31 +0200

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        int a = 35;
+        int b =15;
+        Calculator calculator = new Calculator();
+
+        if(calculator.add(a,b)==(a+b)){
+            System.out.println("test add ok");
+        }
+        else{
+            System.out.println("Error - can not add values");
+        }
+
+        System.out.println("Test - drugi test jednostkowy:");
+
+        if(calculator.substract(a,b)==(a-b)){
+            System.out.println("test substract ok");
+        }
+        else{
+            System.out.println("Error - can not substract values");
+        }
+
+
+
+
     }
 }
