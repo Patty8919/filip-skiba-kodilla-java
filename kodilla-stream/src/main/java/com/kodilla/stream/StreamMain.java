24418c83c6fe147570e91f26a431a95a7cccc25c
filipskiba <filipskiba.247@gmail.com>
2019-06-20 12:09:24 +0200

    Zadanie 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,10 +1,25 @@
 package com.kodilla.stream;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 
-import com.kodilla.stream.person.People;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        People.getList().stream()
+
+        Forum forum = new Forum();
+        Map<Integer, ForumUser>theResultMapOfUsers = forum.getList().stream()
+                .filter(forumUser -> forumUser.getSex()=='M')
+                .filter(forumUser -> forumUser.getDateOfBirth().getYear()<=1999)
+                .filter(forumUser -> forumUser.getPostsCount()>0)
+                .collect(Collectors.toMap(ForumUser::getId,forumUser -> forumUser));
+
+        System.out.println("# elements: " + theResultMapOfUsers.size());
+        theResultMapOfUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .forEach(System.out::println);
     }
+
+
 }
