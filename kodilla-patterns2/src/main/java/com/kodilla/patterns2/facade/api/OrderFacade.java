cf52c5495dec6943e1c9ccf205acc20c54263718
filipskiba <filipskiba.247@gmail.com>
2019-10-19 12:29:59 +0200

    Zadanie 24.3

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -5,12 +5,14 @@ import jdk.nashorn.internal.runtime.options.LoggingOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
-public final class OrderFacade {
+@EnableAspectJAutoProxy
+public class OrderFacade {
     @Autowired
     private ShopService shopService;
     private final static Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
